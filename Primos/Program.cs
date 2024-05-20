d1a538b9fb83d69a7f8118363bee0603e071fdb4
Louis <Louis@LAPTOP-TH69ARB6>
2024-05-20 13:55:28 -0400

    Agregar archivos de proyecto.

---
+++ Primos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Primo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un número:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero == 1)
+            {
+                Console.WriteLine("El número 1 no es primo.");
+                return;
+            }
+
+            int contador = 2;
+            bool primo = true;
+
+            while ((primo) && (contador != numero))
+            {
+                if (numero % contador == 0)
+                    primo = false;
+                contador++;
+            }
+
+            if (primo)
+                Console.WriteLine("El número es primo.");
+            else
+                Console.WriteLine("El número no es primo.");
+        }
+    }
+}
